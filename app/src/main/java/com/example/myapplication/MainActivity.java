d27ff0aae3c8034b85be25adde143f915236dc92
milton927 <milton9272008@gmail.com>
2020-11-12 23:01:31 +0800

    ANDROID

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button switch_text = (Button)findViewById(R.id.btn1);
+        switch_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch_text.setText("Logout");
+            }
+        });
+    }
+}
